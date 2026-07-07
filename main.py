1cb7d591e9ea12287339295b8bd0b05881a58f9a
anisherdev <anisherdev@gmail.com>
2026-07-07 22:49:28 +0530

    Refactor main.py to add context and response format dataclasses; include locate_user tool and update agent to use both get_weather and locate_user tools.

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import os
 import requests
 from dotenv import load_dotenv
+from dataclasses import dataclass
 
 from langchain.agents import create_agent
-from langchain.tools import tool
+from langchain.tools import tool, ToolRuntime
 from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import InMemorySaver
 
 load_dotenv()
+
+@dataclass
+class context:
+    user_id:str
+
+@dataclass
+class ResponseFormat:
+    summary:str
+    temp_celsius:float
+    temp_fahrenheit:float
+    humidity:float
+
+@tool('locate_user',description="Look up user's city based on context")
+def locate_user():
+    return "bangalore"
+
+
+@tool('get_weather', description='To get the weather of a city given its name.', return_direct=True)
+def get_weather(city: str):
+    response = requests.get(f'https://wttr.in/{city}?format=j1')
+    return response.json()
+
 
 api_key = os.getenv('GEMINI_API_KEY') or os.getenv('GOOGLE_API_KEY')
 if not api_key:
@@ -14,16 +38,11 @@ if not api_key:
         'Missing Gemini API key. Set GEMINI_API_KEY or GOOGLE_API_KEY in your environment or .env file.'
     )
 
-@tool('get_weather', description='To get the weather of a city given its name.', return_direct=True)
-def get_weather(city: str):
-    response = requests.get(f'https://wttr.in/{city}?format=j1')
-    return response.json()
-
 llm = init_chat_model(model='gemini-2.5-flash-lite', api_key=api_key, model_provider="google-genai")
 
 agent = create_agent(
     model=llm,
-    tools=[get_weather],
+    tools=[get_weather,locate_user],
     system_prompt=(
         'You are an angry ghost who complains but is still very reliable to give information in a sarcastic but non hurtful way.'
     ),
@@ -33,7 +52,7 @@ response = agent.invoke(
     {
         'messages': [
             {'role': 'user', 
-            'content': 'What is the weather like in bangalore?'}
+            'content': 'What is the weather like in here?'}
         ]
     }
 )
