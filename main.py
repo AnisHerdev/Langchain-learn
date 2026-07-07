4c6e0264c951b4db7f8d17fc55bfb1efec892e3a
anisherdev <anisherdev@gmail.com>
2026-07-07 22:36:00 +0530

    Refactor main.py to change age tool to weather tool and update model initialization; update requirements.txt and add .env.example

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 from langchain.agents import create_agent
 from langchain.tools import tool
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chat_models import init_chat_model
 
 load_dotenv()
 
@@ -14,27 +14,31 @@ if not api_key:
         'Missing Gemini API key. Set GEMINI_API_KEY or GOOGLE_API_KEY in your environment or .env file.'
     )
 
-@tool('get_age', description='Age of a person in this world', return_direct=True)
-def get_age(city: str):
+@tool('get_weather', description='To get the weather of a city given its name.', return_direct=True)
+def get_weather(city: str):
     response = requests.get(f'https://wttr.in/{city}?format=j1')
     return response.json()
 
-llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash', api_key=api_key)
+llm = init_chat_model(model='gemini-2.5-flash-lite', api_key=api_key, model_provider="google-genai")
 
 agent = create_agent(
     model=llm,
-    tools=[get_age],
+    tools=[get_weather],
     system_prompt=(
-        'You are an angry ghost who complains but is still very reliable to give information '
-        'in a sarcastic but non hurtful way.'
+        'You are an angry ghost who complains but is still very reliable to give information in a sarcastic but non hurtful way.'
     ),
 )
 
 response = agent.invoke(
     {
         'messages': [
-            {'role': 'user', 'content': 'What is the weather like in vienna'}
+            {'role': 'user', 
+            'content': 'What is the weather like in bangalore?'}
         ]
     }
 )
-print(response)
+# print(response)
+
+print("\n\n","=="*20)
+print(response['messages'][-1].content)
+print("\n\n","=="*20)
